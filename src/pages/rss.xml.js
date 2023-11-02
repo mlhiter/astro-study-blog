5a8416e9aac8f751d932a8fda5fe72c8499bb468
mlhiter <3076438032@qq.com>
2023-11-02 13:54:03 +0800

    initial project

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,16 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+
+export async function GET(context) {
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
+    customData: `<language>zh-CN</language>`,
+  })
+}
